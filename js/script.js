4537261627394050f8b9534efddaa29378fdf4ac
trimeldawanim@gmail.com <trimeldawanim@gmail.com>
2024-01-06 10:11:31 +0700

    lorem

--- js/script.js
+++ js/script.js
@@ -10,25 +10,6 @@ onclick = () => {
     navbarNav.classList.toggle('active');
 };
 
-// Toggle active search form
-const searchForm = document.querySelector('.search-form');
-const searchBox = document.querySelector('#search-box');
-
-document.querySelector('#search-button').onclick = (e) => {
-  searchForm.classList.toggle('active');
-  searchBox.focus();
-  e.preventDefault();
-};
 
 
-//Click outside bar 
 
-
-const hamburger = document.querySelector 
-('#hamburger-menu');
-
-document.addEventListener('click',function(e) { if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
-    navbarNav.classList.remove('active');
-}
-
-})
